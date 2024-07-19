899cdd18b40140e23922a0479c13046eaf55fc12
Rachelphilipc <rachel.chandrasekaran@tnqtech.com>
2024-07-19 13:48:26 +0530

    Update saucedemo.py

--- saucedemo.py
+++ saucedemo.py
@@ -4,7 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-class SauceDemoLoginTests(unittest.TestCase):
+class TestSuccessfulLogin(unittest.TestCase):
 
     def setUp(self):
         options = Options()
@@ -21,30 +21,6 @@ class SauceDemoLoginTests(unittest.TestCase):
         self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
         self.driver.find_element(By.ID, "login-button").click()
         self.assertEqual(self.driver.current_url, "https://www.saucedemo.com/inventory.html")
-
-    def test_invalid_username(self):
-        self.driver.find_element(By.ID, "user-name").send_keys("invalid_user")
-        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        self.driver.find_element(By.ID, "login-button").click()
-        error_message = self.driver.find_element(By.XPATH, "//h3").text
-        self.assertIn("do not match", error_message)
-
-    def test_invalid_password(self):
-        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
-        self.driver.find_element(By.ID, "password").send_keys("wrong_password")
-        self.driver.find_element(By.ID, "login-button").click()
-        error_message = self.driver.find_element(By.XPATH, "//h3").text
-        self.assertIn("do not match", error_message)
-
-    def test_empty_username(self):
-        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        self.driver.find_element(By.ID, "login-button").click()
-        error_message = self.driver.find_element(By.XPATH, "//h3").text
-        self.assertIn("required", error_message)
-
-    def test_skip_test_no_internet(self):
-        self.skipTest("No internet connection - skipping the test.")
-
 
 if __name__ == "__main__":
     unittest.main()
