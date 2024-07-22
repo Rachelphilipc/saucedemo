70808cf3ed4358cec5a0baceb60c6fefd3d569a5
Rachelphilipc <rachel.chandrasekaran@tnqtech.com>
2024-07-22 12:38:23 +0530

    Update testcases.py

--- testcases.py
+++ testcases.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 options = Options()
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 class SauceDemoTest(unittest.TestCase):
